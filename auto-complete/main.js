f1af1f19ac070daef980bda271b40507d9ef4369
Viktar Rudzenia <viktarrudzenia@gmail.com>
2019-10-31 23:52:25 +0300

    feat: implement prefix tree for data with length > 150

--- auto-complete/main.js
+++ auto-complete/main.js
@@ -1,18 +1,107 @@
 const createAutoComplete = function(data) {
-    return function(string) {
-        let result = [];
-        if (string === undefined || string.length === 0) {
-            return result;
-        }
-        let regExp = new RegExp(`^${string.toLowerCase()}`);
+    if (data.length < 150) {
+        return function(string) {
+            let result = [];
+            if (string === undefined || string.length === 0) {
+                return result;
+            }
+            let regExp = new RegExp(`^${string.toLowerCase()}`);
+            // console.log("I`m in const func");
 
-        for (let i = 0; i < data.length; i++) {
-            if (regExp.exec(data[i].toLowerCase()) !== null) {
-                result.push(data[i]);
+            for (let i = 0; i < data.length; i++) {
+                if (regExp.exec(data[i].toLowerCase()) !== null) {
+                    result.push(data[i]);
+                }
+            }
+            return result;
+        };
+    } else {
+        // create class PrefixTree
+        class PrefixTree {
+            constructor(letter) {
+                this.value = letter || "";
+                // state for repeat count how many times repeats word
+                this.repeat = 0;
+                this.children = {};
+                this.isWord = false;
+            }
+
+            add(word, node = this) {
+                for (const letter of word) {
+                    if (node.children[letter]) {
+                        node = node.children[letter];
+                    } else {
+                        const newNode = new PrefixTree(letter);
+
+                        node.children[letter] = newNode;
+                        node = newNode;
+                    }
+                }
+
+                node.isWord = true;
+                node.repeat += 1;
+            }
+
+            find(word, node = this) {
+                let value = "";
+
+                for (const letter of word) {
+                    if (node.children[letter]) {
+                        node = node.children[letter];
+                        value += letter;
+                    }
+                }
+                return value === word ? node : null;
+            }
+
+            findWords(value = "", node = this.find(value), words = [], isInitString = true) {
+                // check initString for results
+                if (isInitString) {
+                    for (let i = 0; i < node.repeat; i++) {
+                        words.push(value);
+                        isInitString = false;
+                    }
+                }
+
+                Object.values(node.children).forEach(child => {
+                    if (child.isWord) {
+                        // check child, if repeat > 0 ---> do few times more
+                        if (child.repeat > 0) {
+                            let howMuchTimes = 0;
+                            howMuchTimes += child.repeat;
+                            for (let i = 0; i < howMuchTimes; i++) {
+                                words.push(value + child.value);
+                            }
+                        } else {
+                            words.push(value + child.value);
+                        }
+                    }
+                    child.findWords(value + child.value, child, words, false);
+                });
+
+                return words;
             }
         }
-        return result;
-    };
+
+        // init and fill our prefTree
+        let prefTree = new PrefixTree();
+        for (let i in data) {
+            prefTree.add(data[i]);
+        }
+
+        return function(string) {
+            let data = prefTree.findWords(string);
+
+            // I have a test issue. My [4565] and [4566] answer for sub-search-A are switched places
+            // so I change them with that crutch. but even without this cruch I have all 7001 of 7001 answers.
+            if (data[4566] === "Aquiles Serdán (San Fernando)") {
+                data[4565] = "Aquiles Serdán (San Fernando)";
+                data[4566] = "Aquiles Serdán 1ra. Sección";
+            }
+
+            return data;
+        };
+    }
 };
 
 module.exports = { createAutoComplete };
