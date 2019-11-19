d8fea085fb5a615be3c8362da498f4f22fc19bfa
Viktar Rudzenia <viktarrudzenia@gmail.com>
2019-11-19 21:08:39 +0300

    auto-complete-ui/feat: implement working autocomplete UI

--- auto-complete-ui/index.js
+++ auto-complete-ui/index.js
@@ -1,26 +1,57 @@
 import createAutoComplete from "../auto-complete/main.js";
+import { cities } from "./cities.js";
 
-console.log(createAutoComplete);
+let prefixTree = createAutoComplete(cities);
 
-// with function
-// async function getCitiesJSON() {
-//     try {
-//         const cities = await fetch(
-//             "https://raw.githubusercontent.com/Shastel/autocomplete-tests/master/cities.json"
-//         );
-//         if (cities.ok) {
-//             const citiesJSON = await cities.json();
-//             console.log(citiesJSON);
-//         }
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-// getCitiesJSON();
+document.body.onload = addAllElements;
 
-// without function
-const cities = fetch(
-    "https://raw.githubusercontent.com/Shastel/autocomplete-tests/master/cities.json"
-)
-    .then(cities => cities.json())
-    .then(cities => console.log(cities));
+function addAllElements() {
+    let input = document.createElement("input");
+    let divWithHello = document.createElement("div");
+    let divWithResult = document.createElement("div");
+
+    input.id = "input";
+    input.placeholder = "Type here";
+    divWithHello.id = "divWithHello";
+    divWithResult.id = "divWithResult";
+
+    let clearDivWithResult = clear => {
+        if (clear === "not matches") {
+            divWithResult.innerHTML = "No matches. Try find something else";
+        } else if (clear === "all") {
+            divWithResult.innerHTML = "";
+        } else if (clear === "init") {
+            divWithResult.innerHTML = "Result will be here";
+        } else {
+            console.log("SomeError from clearDivWithResult");
+        }
+    };
+
+    divWithHello.innerHTML =
+        "<h1>Hello Stranger! Try find city of your dream here :) Exact string</h1>";
+    divWithResult.innerHTML = "Result will be here";
+
+    document.body.appendChild(divWithHello);
+    document.body.appendChild(input);
+    document.body.appendChild(divWithResult);
+
+    // Track what in input
+    input.oninput = function() {
+        try {
+            if (input.value === "") {
+                clearDivWithResult("init");
+                return;
+            } else {
+                clearDivWithResult("all");
+                let newDiv = document.createElement("div");
+                let result = prefixTree(input.value);
+                let resultLength = result.length;
+                let listOfResult = result.map(element => `<li>${element}</li>\n`).join("");
+                newDiv.innerHTML = `Autocomplete found ${resultLength} element/s <ol>${listOfResult}</ol>`;
+                divWithResult.appendChild(newDiv);
+            }
+        } catch (error) {
+            clearDivWithResult("not matches");
+        }
+    };
+}
