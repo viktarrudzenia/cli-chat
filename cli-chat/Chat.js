c02a9bfee242a1c60a322ce532debd97f73e2dc5
Viktar Rudzenia <viktarrudzenia@gmail.com>
2019-12-06 15:13:29 +0300

    feat: implement colorize for all chat members

--- cli-chat/Chat.js
+++ cli-chat/Chat.js
@@ -3,6 +3,8 @@ const WebSocket = require('ws');
 // const fs = require('fs');
 const chalk = require('chalk');
 const prompts = require('prompts');
+//  const readline = require('readline');
+// readline.moveCursor(process.stdout, 0, -1);
 
 const wsChatURL = 'ws://chat.shas.tel';
 
@@ -51,25 +53,62 @@ const onCancel = (prompt) => {
             second: '2-digit',
         };
 
-        function colorize() {
-            let r = 0;
-            let g = 0;
-            let b = 0;
-            return function () {
-                for (let i = 0; i < 255; i += 1) {
-                    r += 10;
-                    g += 10;
-                    b += 10;
-                    console.log(r, g, b);
-                    return `rgb(${r}, ${g}, ${b})`;
-                }
-            };
+        const colors = [
+            'red',
+            'green',
+            'yellow',
+            'blue',
+            'cyan',
+            'white',
+            'gray',
+        ];
+
+        function generateRGBColor() {
+            const r = Math.floor(Math.random() * 256); // Random between 0-255
+            const g = Math.floor(Math.random() * 256); // Random between 0-255
+            const b = Math.floor(Math.random() * 256); // Random between 0-255
+            return [r, g, b];
         }
 
-        newData.reverse().map((item) => (
-            item.from === response.username
-                ? console.log(`${chalk.blue(new Date(item.time).toLocaleDateString('en-US', timeOptions))} ${chalk.bold.rgb(10, 100, 200)(item.from)}: ${chalk.bold.rgb(10, 100, 200)(item.message)}`)
-                : console.log(`${chalk.blue(new Date(item.time).toLocaleDateString('en-US', timeOptions))} ${chalk.redBright(item.from)}: ${chalk.yellowBright(item.message)}`)));
+        // function generateColorIterator() {
+        //     let currentColor = -1;
+
+        //     return () => {
+        //         if (currentColor >= colors.length - 1) {
+        //             currentColor += 1;
+        //             return colors[currentColor % colors.length];
+        //         }
+        //         currentColor += 1;
+        //         return colors[currentColor];
+        //     };
+        // }
+
+        // const generateColor = generateColorIterator();
+
+        const allUsersWithColors = {
+        };
+
+        newData.reverse();
+
+        for (let i = 0; i < newData.length; i += 1) {
+            // if (newData[i].from === response.username) {
+            //     console.log(`${chalk.blue(new Date(newData[i].time).toLocaleDateString('en-US', timeOptions))} ${chalk.bold.bgGreen.keyword('black')(newData[i].from)}: ${chalk.bgGreen.keyword('black')(newData[i].message)}`);
+            // } else if (allUsersWithColors[newData[i].from] !== undefined) {
+            //     console.log(`${chalk.blue(new Date(newData[i].time).toLocaleDateString('en-US', timeOptions))} ${chalk.bold.keyword(allUsersWithColors[newData[i].from])(newData[i].from)}: ${chalk.bold.keyword(allUsersWithColors[newData[i].from])(newData[i].message)}`);
+            // } else {
+            //     allUsersWithColors[newData[i].from] = generateColor();
+            //     console.log(`${chalk.blue(new Date(newData[i].time).toLocaleDateString('en-US', timeOptions))} ${chalk.bold.keyword(allUsersWithColors[newData[i].from])(newData[i].from)}: ${chalk.bold.keyword(allUsersWithColors[newData[i].from])(newData[i].message)}`);
+            // }
+
+            if (newData[i].from === response.username) {
+                console.log(`${chalk.blue(new Date(newData[i].time).toLocaleDateString('en-US', timeOptions))} ${chalk.bold.bgGreen.keyword('black')(newData[i].from)}: ${chalk.bgGreen.keyword('black')(newData[i].message)}`);
+            } else if (allUsersWithColors[newData[i].from] !== undefined) {
+                console.log(`${chalk.blue(new Date(newData[i].time).toLocaleDateString('en-US', timeOptions))} ${chalk.bold.rgb(...allUsersWithColors[newData[i].from])(newData[i].from)}: ${chalk.bold.rgb(...allUsersWithColors[newData[i].from])(newData[i].message)}`);
+            } else {
+                allUsersWithColors[newData[i].from] = generateRGBColor();
+                console.log(`${chalk.blue(new Date(newData[i].time).toLocaleDateString('en-US', timeOptions))} ${chalk.bold.rgb(...allUsersWithColors[newData[i].from])(newData[i].from)}: ${chalk.bold.rgb(...allUsersWithColors[newData[i].from])(newData[i].message)}`);
+            }
+        }
     });
 
     ws.on('error', (error) => {
@@ -81,13 +120,12 @@ const onCancel = (prompt) => {
     });
 
     for (let i = 0; i < 10e8; i += 1) {
-        const questions = [
-            {
-                type: 'text',
-                name: 'message',
-                message: '',
-                validate: (value) => ((value === '') ? 'Input something please' : true),
-            }];
+        const questions = [{
+            type: 'text',
+            name: 'message',
+            message: '',
+            validate: (value) => ((value === '') ? 'Input something please' : true),
+        }];
 
         const onSubmit = (prompt, answer) => console.log(`Your message: "${chalk.green(answer)}" sended to chat`);
         const onCancel = (prompt) => {
@@ -96,7 +134,6 @@ const onCancel = (prompt) => {
             return false;
         };
         const response2 = await prompts(questions, { onSubmit, onCancel });
-
         const msg = {
             from: response.username,
             message: response2.message,
