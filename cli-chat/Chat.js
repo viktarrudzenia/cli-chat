b1b798cee1ff9f4ab6487d17ee1c6778afbacf87
Viktar Rudzenia <viktarrudzenia@gmail.com>
2019-12-11 14:00:34 +0300

    cli-chat/refactor: move out checkSettings to separate component

--- cli-chat/Chat.js
+++ cli-chat/Chat.js
@@ -1,52 +1,14 @@
 /* eslint-disable no-console */
-const WebSocket = require('ws');
-const fs = require('fs');
-const path = require('path');
 const chalk = require('chalk');
-const prompts = require('prompts');
-const readline = require('readline');
-const notifier = require('node-notifier');
-const TelegramBot = require('./components/TelegramBot/telegramBot');
 const connect = require('./components/ConnectWSChat/connect');
 const auth = require('./components/ConnectWSChat/authorization');
+const checkSettings = require('./components/ConnectWSChat/checkSettings');
 
 const reconnectInterval = 2 * 1000;
 
+const settings = checkSettings();
+const { username, wsChatURL } = settings;
 
-// const allChatData = [];
-
-// ///////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// const settings = fs.readFileSync('telegramBotSettings.config', { flag: 'as+' }, { encoding: 'utf-8' });
-// const regExpUsername = /(?<=USERNAME = ")[^"]+/;
-// const regExpWsChatURL = /(?<=WSCHATURL = ")[^"]+/;
-
-// if (regExpUsername.exec(settings) !== null || regExpWsChatURL.exec(settings !== null)) {
-//     username = regExpUsername.exec(settings)[0];
-//     wsChatURL = regExpWsChatURL.exec(settings)[0];
-// } else {
-//     console.log(chalk.red(`You do not have settings for chat, now you need to answer a couple of questions
-//                     Press enter when you are ready `));
-// }
-
-// ///////////////////////////////////////////////////////////////////////////////////////////////////////
-
-let username;
-let wsChatURL;
-
-const settings = fs.readFileSync(path.join(__dirname, '/settings/settings.config'), { flag: 'as+' }, { encoding: 'utf-8' });
-const regExpUsername = /(?<=USERNAME = ")[^"]+/;
-const regExpWsChatURL = /(?<=WSCHATURL = ")[^"]+/;
-
-if (regExpUsername.exec(settings) !== null || regExpWsChatURL.exec(settings !== null)) {
-    username = regExpUsername.exec(settings)[0];
-    wsChatURL = regExpWsChatURL.exec(settings)[0];
-} else {
-    console.log(chalk.red(`You do not have settings for chat, now you need to answer a couple of questions
-                    Press enter when you are ready `));
-}
-
-// ///////////////////////////////////////////////////////////////////////////////////////////////////////
 (async () => {
     if (!auth.checkIsAuthorized(username, wsChatURL)) {
         const data = await auth.authorization();
---
+++ cli-chat/components/ConnectWSChat/checkSettings.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
+
+function checkSettings() {
+    let username;
+    let wsChatURL;
+    const fileWithSettings = fs.readFileSync(path.join(__dirname, '../../settings/settings.config'), { flag: 'as+' }, { encoding: 'utf-8' });
+    const regExpUsername = /(?<=USERNAME = ")[^"]+/;
+    const regExpWsChatURL = /(?<=WSCHATURL = ")[^"]+/;
+
+    if (regExpUsername.exec(fileWithSettings) !== null
+    || regExpWsChatURL.exec(fileWithSettings !== null)) {
+        username = regExpUsername.exec(fileWithSettings)[0];
+        wsChatURL = regExpWsChatURL.exec(fileWithSettings)[0];
+    } else {
+        console.log(chalk.red(`You do not have settings for chat, now you need to answer a couple of questions
+                    Press enter when you are ready `));
+    }
+    return {
+        username,
+        wsChatURL,
+    };
+}
+
+module.exports = checkSettings;
