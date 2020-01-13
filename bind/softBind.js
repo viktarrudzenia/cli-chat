501a7cd3dad2628f4926fffa107f6b19ffba9521
Viktar Rudzenia <viktarrudzenia@gmail.com>
2020-01-14 00:38:36 +0300

    bind/refactor: left one implementation of softBind

--- bind/softBind.js
+++ bind/softBind.js
@@ -1,10 +1,19 @@
 if (!Function.prototype.softBind) {
     Function.prototype.softBind = function (context) {
+        if (typeof this !== 'function') {
+            throw new TypeError('Not callable');
+        }
         const that = this;
+        const root = window || global;
         const args = [].slice.call(arguments, 1);
-        return function () {
-            return that.apply((!this || this === (window || global)) ? context : this,
-                args.concat([].slice.call(arguments)));
+        const func = function () {};
+        const result = function () {
+            const actualContext = (!this || this === root) ? context : this;
+            const allArgs = args.concat([].slice.call(arguments));
+            return that.apply(actualContext, allArgs);
         };
+        func.prototype = that.prototype;
+        result.prototype = new func();
+        return result;
     };
 }
